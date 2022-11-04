abab962cf6b0be0bb7c9d58c7907e9c73623134a
ackers93 <andrew.ackers@gmail.com>
2022-11-03 21:41:25 -0700

    Intial commit, created user model and incorporated devise

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "home#index"
+
+  Rails.application.routes.draw do
+    devise_scope :user do
+      # Redirests signing out users back to sign-in
+      get "users", to: "devise/sessions#new"
+    end
+  devise_for :users
+  end
+end
