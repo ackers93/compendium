2ff862c59e1d86391e05da11055ff19c857817d0
ackers93 <andrew.ackers@gmail.com>
2022-12-09 14:20:35 -0800

    revised notes to add turbo modals and controllers

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,29 +1,77 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_note, only: %i[ show edit update destroy ]
+  before_action :ensure_frame_response, only: [:new, :edit]
+
+
   def index
     @notes = Note.all
   end
 
   def show
-    @note = Note.find(params[:id])
     @comments = Comment.where(note_id: @note).all
   end
 
   def new
+    @note = Note.new
+  end
+
+  def edit
   end
   
   def create
-    @note = Note.new(title: params['title'], content: params['content'], user_id: current_user.id)
-    if @note.save
-      redirect_to @note
-    else
-      render 'new'
+    puts "NOTEPARAMS #{note_params}"
+    @note = Note.new(note_params)
+    respond_to do |format|
+      if @note.save
+        format.turbo_stream { render turbo_stream: turbo_stream.prepend('notes', partial: 'notes/note', locals: {note: @note}) }
+        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
+        format.json { render :show, status: :created, location: @note }
+      else
+        puts "ERROR #{@note.errors.full_messages}"
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    puts "NOTEEditPARAMS #{note_params}"
+    respond_to do |format|
+      if @note.update(note_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@note, partial: "notes/note", locals: {note: @note}) }
+        format.html { redirect_to note_url(@note), notice: "Note was successfully updated." }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    @note = current_user.notes.find(params[:id])
     @note.destroy
-    redirect_to note_path
+
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: "Note was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def ensure_frame_response
+      return unless Rails.env.development?
+      redirect_to root_path unless turbo_frame_request?
+    end
+
+    # Only allow a list of trusted parameters through.
+    def note_params
+      params.require(:note).permit(:title, :content, :user_id)
+    end
 end
