3571bd439227a788987e5733eb06cf4e940685db
ackers93 <andrew.ackers@gmail.com>
2025-08-22 15:06:30 -0700

    reordering routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root to: "home#index"
 
+  get '/notes/list', to: 'notes#list'
+  get 'bible_verses/books', to: 'bible_verses#book_index'
+  
   resources :notes do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
@@ -11,9 +14,6 @@ Rails.application.routes.draw do
   resources :bible_verses do
     resources :comments, only: [:new, :create, :edit, :update, :destroy]
   end
-
-  get '/notes/list', to: 'notes#list'
-  get 'bible_verses/books', to: 'bible_verses#book_index'
 
   devise_for :users, controllers: {
     sessions: 'users/sessions'
