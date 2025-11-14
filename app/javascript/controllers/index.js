75699bced7759a0af9d4e3f6b3596d423a4fe21c
ackers93 <andrew.ackers@gmail.com>
2025-11-14 14:52:57 -0800

    bible verse picker in notes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,10 @@
 import { application } from "./application"
 import ModalController from "./modal_controller"
 import MobileNavController from "./mobile_nav_controller"
+import BibleVersePickerController from "./bible_verse_picker_controller"
 
 application.register("modal", ModalController)
 application.register("mobile-nav", MobileNavController)
+application.register("bible-verse-picker", BibleVersePickerController)
 
 export { application } 
